69e673c2e7b47df873a31c32962fb032d785b77e
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2022-09-07 11:31:14 +0530

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -14,7 +14,7 @@ if st.button('Add'):
     l[a]=b
 
 st.subheader("Delete")
-a=st.text_input("Enter the Label to delete","",key=1)
+a=st.text_input("Enter the Label to delete","",key=3)
 if st.button('Delete'):
     if a=="" :
         pass
